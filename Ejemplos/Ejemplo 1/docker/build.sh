bec16de53ed24660d05cf4deff5ec2008319b40a
Isaac Araya Solano <isaacaraya@estudiantec.cr>
2023-02-21 11:58:15 -0600

    uploaded information and examples

---
+++ Ejemplos/Ejemplo 1/docker/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo docker login
+cd componente1
+sudo docker build -t nereo08/componente1 .
+sudo docker images
+sudo docker push nereo08/componente1
+cd ..
+cd componente2
+sudo docker build -t nereo08/componente2 .
+sudo docker images
+sudo docker push nereo08/componente2
+cd ..
+cd componente3
+sudo docker build -t nereo08/componente3 .
+sudo docker images
+sudo docker push nereo08/componente3
+cd ..
+cd componente4
+sudo docker build -t nereo08/componente4 .
+sudo docker images
+sudo docker push nereo08/componente4
